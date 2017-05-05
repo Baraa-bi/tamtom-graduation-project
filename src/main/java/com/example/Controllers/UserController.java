64b83acedaf474d4765d875b933d01101036b592
omar al-tamimi <tamimi.o@opensooq.com>
2017-05-06 02:55:25 +0300

    user controller fixes

--- src/main/java/com/example/Controllers/UserController.java
+++ src/main/java/com/example/Controllers/UserController.java
@@ -22,8 +22,6 @@ import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashSet;
 
 /**
  * Created by baraa on 4/3/2017.
@@ -51,20 +49,20 @@ public class UserController {
     }
 
     @RequestMapping("/createUser")
-    public void createUser(HttpServletResponse response,@ModelAttribute User user) throws IOException {
-        if(userRepo.findByUserEmail(user.getUserEmail())!=null)
-            response.sendError(400,"Already Exists");
+    public void createUser(HttpServletResponse response, @ModelAttribute User user) throws IOException {
+        if (userRepo.findByUserEmail(user.getUserEmail()) != null)
+            response.sendError(400, "Already Exists");
         else
-            response.sendRedirect("/getUser?userId="+userRepo.save(user).getUserId());
-        }
+            response.sendRedirect("/getUser?userId=" + userRepo.save(user).getUserId());
+    }
 
 
     @RequestMapping("/createUserLocation")
-    public void createLocation(@ModelAttribute Location location,HttpServletResponse response) throws IOException {
-        if(userRepo.findOne(location.getUserId())==null)
-            response.sendError(400,"User Not Exist");
+    public void createLocation(@ModelAttribute Location location, HttpServletResponse response) throws IOException {
+        if (userRepo.findOne(location.getUserId()) == null)
+            response.sendError(400, "User Not Exist");
         else
-            response.sendRedirect("/getUser?userId="+locationRepo.save(location).getUserId());
+            response.sendRedirect("/getUser?userId=" + locationRepo.save(location).getUserId());
     }
 
     @RequestMapping("/removeUserLocation")
@@ -72,177 +70,169 @@ public class UserController {
                                @RequestParam Long locationId,
                                HttpServletResponse response) throws IOException {
         Location location = locationRepo.findOne(locationId);
-        if (location == null)
-        {
+        if (location == null) {
             response.sendError(400, "Location Not Exist");
             return;
         }
-        if (location.getUserId() == userId)
-        {
+        if (location.getUserId() == userId) {
             response.sendError(400, "User Not Match This Location Id");
             return;
-        }
-        else {
+        } else {
             locationRepo.delete(locationId);
             response.sendRedirect("/getUser?userId=" + locationRepo.save(location).getUserId());
         }
     }
 
 
-    @RequestMapping(value = "/getUser",produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
-    public String getUser(HttpServletRequest request,HttpServletResponse response,@RequestParam("userId") long id) throws JSONException, IOException {
+    @RequestMapping(value = "/getUser", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    public String getUser(HttpServletRequest request, HttpServletResponse response, @RequestParam("userId") long id) throws JSONException, IOException {
         User user = userRepo.findOne(id);
-        if(user==null)
-        {
-            response.sendError(400,"user not Exisit");
-            return"";
+        if (user == null) {
+            response.sendError(400, "user not Exisit");
+            return "";
         }
         JSONArray locations = new JSONArray();
         JSONObject object = new JSONObject(user);
-        if(user.getFavoriteIds()!=null) {
+        if (user.getFavoriteIds() != null) {
             JSONArray favIds = new JSONArray();
-            for(String lineIds : user.getFavoriteIds().split(",")){
-                Long lineId =Long.parseLong(lineIds);
+            for (String lineIds : user.getFavoriteIds().split(",")) {
+                Long lineId = Long.parseLong(lineIds);
                 RestTemplate template = new RestTemplate();
-                favIds.put(new JSONObject(template.getForObject(getHost(request)+"/getLine?lineId="+lineId,String.class)));
+                favIds.put(new JSONObject(template.getForObject(getHost(request) + "/getLine?lineId=" + lineId, String.class)));
             }
             object.remove("favoriteIds");
             object.put("favoriteLines", favIds);
-            object.put("favoriteLinesSize",favIds.length());
-        } for(Location location : (locationRepo.findByUserId(id))) {
+            object.put("favoriteLinesSize", favIds.length());
+        }
+        for (Location location : (locationRepo.findByUserId(id))) {
             JSONObject object1 = new JSONObject();
             JSONArray latLng = new JSONArray();
             JSONObject points = new JSONObject();
-            points.put("latitude",location.getLatitude());
-            points.put("longitude",location.getLongitude());
+            points.put("latitude", location.getLatitude());
+            points.put("longitude", location.getLongitude());
             latLng.put(points);
             JSONArray days = new JSONArray(location.getDays().split(","));
-            object1.put("time",location.getTime());
-            object1.put("days",days);
-            object1.put("LatLng",latLng);
+            object1.put("time", location.getTime());
+            object1.put("days", days);
+            object1.put("LatLng", latLng);
             locations.put(object1);
         }
-        object.put("locations",locations);
-        object.put("locationsSize",locations.length());
+        object.put("locations", locations);
+        object.put("locationsSize", locations.length());
         return object.toString();
     }
 
-    @RequestMapping(value = "/getAllUsers",produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    @RequestMapping(value = "/getAllUsers", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
     public String getAllUsers(HttpServletRequest request) throws JSONException, MalformedURLException {
         JSONArray usersArray = new JSONArray();
         for (User user : userRepo.findAll()) {
             RestTemplate template = new RestTemplate();
-            usersArray.put(new JSONObject(template.getForObject(getHost(request)+"/getUser?userId="+user.getUserId(),String.class)));
+            usersArray.put(new JSONObject(template.getForObject(getHost(request) + "/getUser?userId=" + user.getUserId(), String.class)));
         }
         return usersArray.toString();
     }
 
     @RequestMapping(value = "/userLogin")
-    public void driverLogin(HttpServletResponse response , @RequestParam("userEmail")String userEmail,
-                              @RequestParam("userPassword")String userPassword) throws IOException {
+    public void driverLogin(HttpServletResponse response, @RequestParam("userEmail") String userEmail,
+                            @RequestParam("userPassword") String userPassword) throws IOException {
         User user = userRepo.findByUserEmail(userEmail);
-        if(user==null)
-        {
-            response.sendError(400,"User Not Exist");
+        if (user == null) {
+            response.sendError(400, "User Not Exist");
             return;
         }
-        if(user.getUserPassword().equals(userPassword))
-        {
-            response.sendRedirect("/getUser?userId="+user.getUserId());
+        if (user.getUserPassword().equals(userPassword)) {
+            response.sendRedirect("/getUser?userId=" + user.getUserId());
             return;
-        }
-        else
-            response.sendError(400,"Invalid User Password");
+        } else
+            response.sendError(400, "Invalid User Password");
     }
 
 
     @RequestMapping("/addUserFavoriteLine")
     public void addBusToLine(HttpServletResponse response,
-                               @RequestParam("lineId")Long lineId,
-                               @RequestParam("userId")Long userId) throws IOException {
-        if(lineRepo.findOne(lineId)!=null)
-        {
+                             @RequestParam("lineId") Long lineId,
+                             @RequestParam("userId") Long userId) throws IOException {
+        if (lineRepo.findOne(lineId) != null) {
             User user = userRepo.findOne(userId);
-            if(user.getFavoriteIds()==null)
-                user.setFavoriteIds(lineId+",");
+            if (user.getFavoriteIds() == null)
+                user.setFavoriteIds(lineId + ",");
             else
-                user.setFavoriteIds(user.getFavoriteIds()+lineId+",");
+                user.setFavoriteIds(user.getFavoriteIds() + lineId + ",");
             userRepo.save(user);
-            response.sendRedirect("/getUser?userId="+userId);
-        }
-        else
-        response.sendError(400,"Line Not Exist");
+            response.sendRedirect("/getUser?userId=" + userId);
+        } else
+            response.sendError(400, "Line Not Exist");
     }
 
     @RequestMapping("/removeUserFavoriteLine")
     public void removeBusToLine(HttpServletResponse response,
-                             @RequestParam("lineId")Long lineId,
-                             @RequestParam("userId")Long userId) throws IOException {
-        if(lineRepo.findOne(lineId)!=null)
-        {
+                                @RequestParam("lineId") Long lineId,
+                                @RequestParam("userId") Long userId) throws IOException {
+        if (lineRepo.findOne(lineId) != null) {
             User user = userRepo.findOne(userId);
-            if(user.getFavoriteIds().split(",").length==1)
+            if (user.getFavoriteIds().split(",").length == 1)
                 user.setFavoriteIds(null);
             else {
-                HashSet<String> mySet = new HashSet<String>(Arrays.asList(user.getFavoriteIds().split(",")));
-                mySet.remove(lineId);
-                String favorite = mySet.toString();
-                user.setFavoriteIds(favorite.substring(1, favorite.length() - 1));
+                String[] ar = user.getFavoriteIds().split(",");
+                String favorite = null;
+                for (int i = 0; i < ar.length; i++) {
+                    if (String.valueOf(lineId).equals(ar[i]))
+                        continue;
+                    else {
+                        favorite += ar[i] + ",";
+                    }
+                }
+                user.setFavoriteIds(favorite);
             }
 
             userRepo.save(user);
             response.sendRedirect("/getUser?userId=" + userId);
-        }
-        else
-            response.sendError(400,"Line Not Exist");
+        } else
+            response.sendError(400, "Line Not Exist");
     }
+
     @RequestMapping("removeUser")
-    public void removeUser(@RequestParam Long userId,HttpServletResponse response) throws IOException {
-        if (userRepo.findOne(userId)==null)
-            response.sendError(400,"userId not Exist");
-        else
-        {
+    public void removeUser(@RequestParam Long userId, HttpServletResponse response) throws IOException {
+        if (userRepo.findOne(userId) == null)
+            response.sendError(400, "userId not Exist");
+        else {
             userRepo.delete(userId);
             response.getWriter().write("User Has Been Removed Successfully");
         }
     }
 
     @RequestMapping("/updateUser")
-    public void updateUser (HttpServletResponse response,@ModelAttribute User user) throws IOException
-    {
-        if(user.getUserId()==null)
-        {
-            response.sendError(400,"userId Not Exist");
-        return;
-        }
-        User dbUser = userRepo.findOne(user.getUserId());
-        if (dbUser==null)
-        {
-            response.sendError(400,"userId Not Exist");
+    public void updateUser(HttpServletResponse response, @ModelAttribute User user) throws IOException {
+        if (user.getUserId() == null) {
+            response.sendError(400, "userId Not Exist");
             return;
         }
-        else{
-            dbUser.setUserPassword(user.getUserPassword()!=null?user.getUserPassword():dbUser.getUserPassword());
-            dbUser.setFavoriteIds(user.getFavoriteIds()!=null?user.getFavoriteIds():dbUser.getFavoriteIds());
-            dbUser.setUserEmail(user.getUserEmail()!=null?user.getUserEmail():dbUser.getUserEmail());
+        User dbUser = userRepo.findOne(user.getUserId());
+        if (dbUser == null) {
+            response.sendError(400, "userId Not Exist");
+            return;
+        } else {
+            dbUser.setUserPassword(user.getUserPassword() != null ? user.getUserPassword() : dbUser.getUserPassword());
+            dbUser.setFavoriteIds(user.getFavoriteIds() != null ? user.getFavoriteIds() : dbUser.getFavoriteIds());
+            dbUser.setUserEmail(user.getUserEmail() != null ? user.getUserEmail() : dbUser.getUserEmail());
             userRepo.save(dbUser);
-            response.sendRedirect("/getUser?userId="+dbUser.getUserId());
+            response.sendRedirect("/getUser?userId=" + dbUser.getUserId());
         }
     }
 
 
-    @RequestMapping(value = "/searchUsers",produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
-    public String searchUsers(HttpServletRequest request,@RequestParam ArrayList<Long> usersIds,@RequestParam String days , @RequestParam(required = false) String time) throws MalformedURLException, JSONException {
+    @RequestMapping(value = "/searchUsers", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    public String searchUsers(HttpServletRequest request, @RequestParam ArrayList<Long> usersIds, @RequestParam String days, @RequestParam(required = false) String time) throws MalformedURLException, JSONException {
         JSONArray usersArray = new JSONArray();
-        if(time==null)
-        for (User user : userRepo.search(usersIds,days)) {
-            RestTemplate template = new RestTemplate();
-            usersArray.put(new JSONObject(template.getForObject(getHost(request)+"/getUser?userId="+user.getUserId(),String.class)));
-        }
-        else
-            for (User user : userRepo.search(usersIds,days)) {
+        if (time == null)
+            for (User user : userRepo.search(usersIds, days)) {
                 RestTemplate template = new RestTemplate();
-                usersArray.put(new JSONObject(template.getForObject(getHost(request)+"/getUser?userId="+user.getUserId(),String.class)));
+                usersArray.put(new JSONObject(template.getForObject(getHost(request) + "/getUser?userId=" + user.getUserId(), String.class)));
+            }
+        else
+            for (User user : userRepo.search(usersIds, days, time)) {
+                RestTemplate template = new RestTemplate();
+                usersArray.put(new JSONObject(template.getForObject(getHost(request) + "/getUser?userId=" + user.getUserId(), String.class)));
             }
 
         return usersArray.toString();
